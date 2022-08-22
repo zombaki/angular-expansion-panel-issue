2c642e7b08ad259d40428190cd8c714ae7c0577f
zombaki <piyush.nath.2272@gmail.com>
2022-08-22 13:48:29 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-mf8ux6
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-mf8ux6)
---
+++ src/app/expansion-overview-example.ts
@@ -0,0 +1,18 @@
+import {Component} from '@angular/core';
+
+/**
+ * @title Basic expansion panel
+ */
+@Component({
+  selector: 'expansion-overview-example',
+  templateUrl: 'expansion-overview-example.html',
+  styleUrls: ['expansion-overview-example.css'],
+})
+export class ExpansionOverviewExample {
+
+}
+
+
+/**  Copyright 2022 Google LLC. All Rights Reserved.
+    Use of this source code is governed by an MIT-style license that
+    can be found in the LICENSE file at https://angular.io/license */
